6790a7b208332dff64f3736f56f10ba68b31666b
UmmIt <git@ummit.dev>
2026-01-02 00:21:17 +0800

    refactor: removing hyprswitch entries from menu.sh

--- script/misc/restart/menu.sh
+++ script/misc/restart/menu.sh
@@ -17,10 +17,9 @@ while true; do
     echo -e "${CYAN}$(printf '%.0s-' {1..40})${NC}"
     echo -e "${YELLOW}1) Restart/Start swaync${NC}"
     echo -e "${YELLOW}2) Restart/Start waybar${NC}"
-    echo -e "${YELLOW}3) Restart/Start hyprswitch${NC}"
-    echo -e "${YELLOW}4) Restart/Start waybar and swaync${NC}"
-    echo -e "${YELLOW}5) Kill waybar, swaync and hyprswitch${NC}"
-    echo -e "${YELLOW}6) Exit${NC}"
+    echo -e "${YELLOW}3) Restart/Start ALL${NC}"
+    echo -e "${YELLOW}4) Kill waybar, swaync${NC}"
+    echo -e "${YELLOW}5) Exit${NC}"
     echo -e "${CYAN}$(printf '%.0s-' {1..40})${NC}"
 
     read -p "$(echo -e ${DARK_RED}Enter your choice: ${NC})" choice
@@ -33,15 +32,12 @@ while true; do
             source ./waybar-restart.sh
             ;;
         3)
-            source ./hyprswitch-restart.sh
+            source ./waybar-restart.sh && source ./swaync-restart.sh
             ;;
         4)
-            source ./waybar-restart.sh && source ./swaync-restart.sh && ./hyprswitch-restart.sh
-            ;;
-        5)
             source ./kill.sh
             ;;
-        6)
+        5)
             echo -e "${GREEN}Exiting...${NC}"
             break
             ;;
